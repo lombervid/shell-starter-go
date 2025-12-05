9cf35080196e762370a1aab94770edd65804f3fa
lombervid <david.lomberlan@gmail.com>
2025-12-05 15:16:43 -0600

    handle invalid commands

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		panic("error")
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
