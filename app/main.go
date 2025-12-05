56d3852e2f4356d6d2ae862239ccc60d30f08c4a
lombervid <david.lomberlan@gmail.com>
2025-12-05 15:26:47 -0600

    refactor: better termination

--- app/main.go
+++ app/main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	fmt.Print("$ ")
+
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-
 	if err != nil {
-		panic("error")
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
 	}
-
 	fmt.Println(command[:len(command)-1] + ": command not found")
 }
