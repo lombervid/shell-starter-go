62645f7b5c18d74e6848ae715d69c83c08748b17
lombervid <david.lomberlan@gmail.com>
2025-12-05 16:31:18 -0600

    implement `echo` command

--- app/main.go
+++ app/main.go
@@ -4,21 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func repl() {
 	fmt.Print("$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	inputString, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
 		os.Exit(1)
 	}
 
-	command = command[:len(command)-1]
+	inputString = strings.TrimSpace(inputString)
+	command, cmdArgs, _ := strings.Cut(inputString, " ")
+	cmdArgs = strings.TrimSpace(cmdArgs)
+
 	switch command {
 	case "exit":
 		os.Exit(0)
+	case "echo":
+		fmt.Println(cmdArgs)
 	default:
 		fmt.Println(command + ": command not found")
 	}
